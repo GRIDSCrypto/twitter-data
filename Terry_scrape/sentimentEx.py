6b61f7e0096eb22277e1e9449f0a0420b3820b8c
tchia007 <tchia007@ucr.edu>
2018-04-07 15:20:24 -0700

    google cloud sentiment analysis script

---
+++ Terry_scrape/sentimentEx.py
@@ -0,0 +1,50 @@
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+import time
+import csv
+
+# Instantiates a client
+client = language.LanguageServiceClient()
+
+tweets_body = [] # array for storing the body of the tweets
+tweets_timestamp = [] #array for storing timestamps of the tweets
+
+#open csv file to gather the tweet and timestamp into arrays
+with open('btc_5_2017.csv', 'r') as csvfile:
+	reader = csv.reader(csvfile)
+	for row in reader:
+		body = row[5]
+		timestamp = row[1]
+		if body != "body":
+			tweets_body.append(body)
+		if timestamp != "timestamp":
+			tweets_timestamp.append(timestamp)
+
+#writing everything into a new file to write with sentiment score 
+#columns will be timestamp, text, sentiment score
+#sentiment score ranges from -1 to 1 
+with open('btc_5_2017_gcp_sentiment.csv', 'w') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerow(["timestamp", "tweet_text", "sentiment score"])
+	counter = 0
+	for body in tweets_body:
+		# The text to analyze
+		text = body
+		try:
+			document = types.Document(content=text,type=enums.Document.Type.PLAIN_TEXT)
+
+			# Detects the sentiment of the text
+			sentiment = client.analyze_sentiment(document=document).document_sentiment
+		except Exception:
+			pass
+
+		#writing to csv now
+		writer.writerow([text, sentiment.score])
+		counter += 1
+
+		#sleeping due to 600 request per minute quota by google
+		if counter % 550 == 0:
+			print("sleeping")
+			time.sleep(60)
